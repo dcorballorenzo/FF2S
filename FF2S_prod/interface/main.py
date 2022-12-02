c18e8416c4f92289f2057fa09fc61d423e8cc626
Sebastien Moreau <sebastien.moreau0209@gmail.com>
2022-12-02 16:11:17 +0100

    WIP ML Engineering

---
+++ FF2S_prod/interface/main.py
@@ -0,0 +1,5 @@
+if __name__=="__main__":
+    pass
+    #Load sample of photos
+    #Load corresponding sample of sketches
+    #-> Retravailler la fonction load images (le faire à partir de listes)
